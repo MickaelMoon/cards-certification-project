e38f6a0643ee0186c27b4f7cee87c3b76b1fe781
MickaelMoon <mickael.saintmartin@outlook.fr>
2025-01-27 11:58:13 +0100

    + providers to _app.tsx and dependencies

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { config } from "@/lib/wagmi";
+import {WagmiProvider} from "wagmi";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+                <ChakraProvider value={defaultSystem}>
+                    <Component {...pageProps} />
+                </ChakraProvider>
+            </QueryClientProvider>
+      </WagmiProvider>
+  );
 }
