1ebe2f855ccf815d7dc58df673f35fc8bdc1f439
MickaelMoon <mickael.saintmartin@outlook.fr>
2025-01-31 13:58:39 +0100

    + add ui menu component ~ refactored walletconnectionbuttons component

--- frontend/src/components/WalletConnectionButtons.tsx
+++ frontend/src/components/WalletConnectionButtons.tsx
@@ -1,6 +1,12 @@
 import {Button, Box, Text} from "@chakra-ui/react";
 import { useAccount, useConnect, useDisconnect } from 'wagmi';
 import {Tooltip} from "@/components/ui/tooltip";
+import {
+    MenuContent,
+    MenuItem,
+    MenuRoot,
+    MenuTrigger,
+} from "@/components/ui/menu"
 
 export default function WalletConnectionButtons() {
     const { address, isConnected } = useAccount();
@@ -13,9 +19,20 @@ export default function WalletConnectionButtons() {
                 <Button colorPalette="blue" onClick={() => connect({ connector: connectors[0] })}>Connecter le wallet</Button>
             ) : (
                 <Box maxWidth="200px" display="flex" justifyContent="center" flexDirection="column">
-                    <Button colorPalette="red" onClick={() => disconnect?.()}>Déconnecter le wallet</Button>
+                    <MenuRoot>
+                        <MenuTrigger asChild>
+                            <Button variant="outline" size="sm">
+                                Menu
+                            </Button>
+                        </MenuTrigger>
+                        <MenuContent>
+                            <MenuItem value="new-file">New File...</MenuItem>
+                            <MenuItem value="new-win">New Window</MenuItem>
+                            <MenuItem color="fg.error" value="disconnect" onClick={() => disconnect?.()}>Déconnecter le wallet</MenuItem>
+                        </MenuContent>
+                    </MenuRoot>
                     <Tooltip showArrow content={address} interactive>
-                        <Text truncate>Connecté avec : {address}</Text>
+                        <Text cursor="pointer" truncate>Connecté avec : {address}</Text>
                     </Tooltip>
                 </Box>
             )}
