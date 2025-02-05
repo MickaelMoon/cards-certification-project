1f2ba8d6ce3e69f519415985f0baf57e2ce9c87c
MickaelMoon <mickael.saintmartin@outlook.fr>
2025-02-05 15:24:49 +0100

    ~Modified footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 export default function Footer() {
     return (
-        <Box as="footer" display="flex" justifyContent="center" alignItems="center" padding="1rem">
+        <Box as="footer" display="flex" justifyContent="center" alignItems="center" padding="1rem" height="6rem">
             <Text>&copy; 2025 Your Company. All rights reserved.</Text>
         </Box>
     );
